c54aad7100f092bd44e8a26418a271ba34a932e9
Franco Perez <frankperman03@gmail.com>
2022-08-08 14:42:51 -0500

    first commit

---
+++ src/GIFExpertApp.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+
+// Functional component
+export const GIFExpertApp = () => {
+    return (
+        <>
+            <h1>Gif Expert App (React Vite) </h1>
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GIFExpertApp } from './GIFExpertApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GIFExpertApp />
+  </React.StrictMode>
+)
