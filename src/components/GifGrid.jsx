336e5913cb545185e5186277f15b8c0a42fcd9e8
Franco Perez <frankperman03@gmail.com>
2022-09-08 21:10:26 -0500

    añadiendo custom hook para gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
-import {getGifi} from '../helpers/getGifi'
 import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from './hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async() => {
-      const newImages = await getGifi(category); 
-      setImages(newImages); 
-    }
-
-    useEffect( () => {     
-      getImages();
-    },[])
+    const {images, isLoading} = useFetchGifs(category);
     
 
   return (
---
+++ src/components/hooks/useFetchGifs.js
@@ -0,0 +1,25 @@
+// CUSTOM HOOKS
+
+import { useEffect, useState } from "react";
+import { getGifi } from "../../helpers/getGifi";
+
+export const useFetchGifs = ( category ) => {
+  
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getImages = async() => {
+      const newImages = await getGifi(category); 
+      setImages(newImages); 
+    }
+
+    useEffect( () => {     
+      getImages();
+    },[])
+
+    return {
+        images,
+        isLoading
+    }
+
+}
